3e1a07ec5fac77aa5b1547b9bc590a7d85f4c458
hmursaleen <habibulmursaleen@gmail.com>
2023-07-07 01:35:34 +0600

    creating core app done

--- eshop/core/urls.py
+++ eshop/core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.frontpage, name = 'frontpage'),
+	path('about/', views.about, name = 'about'),
 ]
--- eshop/core/views.py
+++ eshop/core/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def frontpage(request):
 	return render(request, 'core/frontpage.html')
+
+def about(request):
+	return render(request, 'core/about.html')
