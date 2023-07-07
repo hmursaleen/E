b3433f71988cc2841a247b5a6ab943f7056040fd
hmursaleen <habibulmursaleen@gmail.com>
2023-07-07 12:12:55 +0600

    adding newest product is done

--- eshop/core/views.py
+++ eshop/core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from store.models import Product
+#i want to show all newest products, so i've to import this 
 
 def frontpage(request):
-	return render(request, 'core/frontpage.html')
+	products = Product.objects.all()[0:6] #want to show first six products
+	return render(request, 'core/frontpage.html', {
+		'products' : products,
+		})
 
 def about(request):
 	return render(request, 'core/about.html')
