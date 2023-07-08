9713e2af82e799ac224272aa94330b5d51f41578
hmursaleen <habibulmursaleen@gmail.com>
2023-07-09 01:15:39 +0600

    added requirements.txt, add products functionality, but not working right yet

--- eshop/userprofile/views.py
+++ eshop/userprofile/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from django.utils.text import slugify
 from . models import Userprofile
 # Create your views here.
+
+from store.forms import ProductForm
 
 def vendor_detail(request, pk):
 	user = User.objects.get(pk=pk)
@@ -12,6 +15,34 @@ def vendor_detail(request, pk):
 		'user' : user,
 		})
 
+@login_required
+def mystore(request):
+	return render(request, 'userprofile/mystore.html')
+
+
+@login_required
+def add_product(request):
+	if request.method == 'POST':
+		form = ProductForm(request.POST, request.FILES)
+
+		if form.is_valid():
+			title = request.POST.get('title')
+			product = form.save(commit=False)
+			product.user = request.user 
+			product.slug = slugify(title)
+			product.save()
+
+			return redirect('mystore')
+
+	else:
+		form = ProductForm()
+
+	return render(request, 'userprofile/add_product.html', {
+		'form' : form,
+		})
+
+
+@login_required
 def myaccount(request):
 	return render(request, 'userprofile/myaccount.html')
 
